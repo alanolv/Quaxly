aa5b3e1403a32dddc6eaf44859df221414b37e10
alanolv <olvera.cristino.alan@gmail.com>
2024-04-20 00:41:37 -0600

    add deliver button

--- quaxly/lib/widgets/track_card.dart
+++ quaxly/lib/widgets/track_card.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'package:quaxly/widgets/title_text.dart';
 import 'package:get/get.dart';
-import 'package:quaxly/widgets/action_button.dart';
+import 'package:quaxly/widgets/action_short_button.dart';
 
 class TrackCard extends StatefulWidget {
   final String imagePath;
   final String deliveryId;
   final bool isDelivered;
+  final Function() onTapFunction;
 
   const TrackCard(
       {Key? key,
       required this.imagePath,
       required this.deliveryId,
+      required this.onTapFunction,
       this.isDelivered = false})
       : super(key: key);
 
@@ -68,18 +70,22 @@ class _TrackCardState extends State<TrackCard> {
               Row(
                 children: [
                   Container(
-                    margin: const EdgeInsets.only(top: 15, left: 10),
+                    margin: const EdgeInsets.only(top: 10, left: 10),
                     child: GestureDetector(
                       onTap: () {
                         // Agrega la lógica del evento de toque aquí
                       },
                       child: Image.asset(
                         'assets/images/Route-button.png',
-                        width: 76,
-                        height: 73,
+                        width: 86,
+                        height: 83,
                       ),
                     ),
                   ),
+                  ActionShortButton(
+                        text: 'deliver'.tr, 
+                        onTapFunction: widget.onTapFunction, 
+                        )
                 ],
               ),
             ],
